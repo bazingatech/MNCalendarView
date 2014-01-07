2333d95bc79306bda7faea55ca303d37234abf0a
Johnny Tsai <johnny.tsai@mybazinga.com>
2014-01-07 15:24:43 -0800

    Removing snapping effect and changing calendar duration to 5 years from current date.

--- MNCalendarView/MNCalendarView.m
+++ MNCalendarView/MNCalendarView.m
@@ -43,7 +43,7 @@
     self.backgroundColor = [UIColor colorWithRed:0.682 green:0.549 blue:0.761 alpha:1.0];
     self.calendar        = NSCalendar.currentCalendar;
     self.fromDate        = [NSDate.date mn_beginningOfDay:self.calendar];
-    self.toDate          = [self.fromDate dateByAddingTimeInterval:MN_YEAR * 4];
+    self.toDate          = [self.fromDate dateByAddingTimeInterval:MN_YEAR * 5];
     self.daysInWeek      = 7;
     
     self.headerViewClass  = MNCalendarHeaderView.class;
